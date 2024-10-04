cfff3d8c87f57b56c47a98b904fb37ba9aafb01a
r <reedhdonaldson@gmail.com>
2024-10-04 00:56:35 -0700

    plan9port font overlay

--- flake.nix
+++ flake.nix
@@ -60,6 +60,7 @@
             inherit inputs;
           };
           modules = [
+            (./overlays)
             # Set Git commit hash for darwin-version.
             {
               system.configurationRevision = self.rev or self.dirtyRev or null;
--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -6,6 +6,7 @@
 
   home.packages = with pkgs; [
     d2
+    plan9port
   ];
 
   services = {
