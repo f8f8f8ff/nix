2abfbc9ad476ef4a5494b13e4fd890dffd5154d6
reed donaldson <reedhdonaldson@gmail.com>
2024-11-05 19:43:20 -0800

    fix discordsend hash

--- pkgs/discordsend.nix
+++ pkgs/discordsend.nix
@@ -11,7 +11,7 @@ buildGoModule {
     owner = "f8f8f8ff";
     repo = "discord";
     rev = version;
-    hash = "sha256-X4vMwp5CWGqNLwBDC7hl4C1H1JKrZVAzLM0AmQMtJ9Q=";
+    hash = "sha256-MbOkoEfOVHVLnXWCx1DSAewXXF0Qqnk8nMh3cu/Lgbg=";
   };
 
   vendorHash = null;
